6a2a7310cfc68d9a95f4b4820096f8c0ca3728a0
Rohit Sinha <rohit@zkbricks.com>
2024-12-10 13:19:47 -0500

    proper error handling

--- src/crypto/ppss/jkkx16.rs
+++ src/crypto/ppss/jkkx16.rs
@@ -1,26 +1,60 @@
 //! Implements the construction from https://eprint.iacr.org/2016/144.pdf (Figure 7)
 
 #![allow(dead_code, unused)]
-use super::PpssPcheme;
-use ark_crypto_primitives::Error;
-use ark_std::ops::*;
-use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
-use ark_std::rand::Rng;
-use ark_std::{Zero, hash::Hash, marker::PhantomData, vec::Vec};
 
-use ark_bls12_381::g1::Config as G1Config;
-use ark_bls12_381::g2::Config as G2Config;
-use ark_bls12_381::{Fq2, G1Affine, G2Affine, G1Projective, G2Projective};
-use ark_bls12_381::{Bls12_381, Fq, Fr};
-use ark_ec::{CurveGroup, CurveConfig, AffineRepr, short_weierstrass::{Affine, Projective}};
-use ark_ec::hashing::{HashToCurve, curve_maps::wb::WBMap, map_to_curve_hasher::MapToCurveBasedHasher};
+use std::fmt;
+use ark_crypto_primitives::Error;
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use ark_std::{ops::*, Zero, rand::Rng, hash::Hash, marker::PhantomData, vec::Vec};
+use ark_bls12_381::{
+    g1::Config as G1Config,
+    g2::Config as G2Config,
+    G1Affine, G2Affine, G1Projective, G2Projective,
+    Bls12_381, Fq, Fq2, Fr
+};
+use ark_ec::{
+    CurveGroup, CurveConfig, AffineRepr, short_weierstrass::{Affine, Projective},
+    hashing::{HashToCurve, HashToCurveError, curve_maps::wb::WBMap, map_to_curve_hasher::MapToCurveBasedHasher}
+};
 use ark_ff::{Field, fields::PrimeField, UniformRand, field_hashers::DefaultFieldHasher, BigInteger256};
 
 use blake2::Blake2s;
 use digest::Digest;
 use sha2::Sha256;
 
+use super::PpssPcheme;
 use super::sss;
+
+/// Error enum to wrap underlying failures in arkworks operations, or wrap errors from dependencies.
+/// Inspired by this excellent post: <https://blog.burntsushi.net/rust-error-handling>
+#[derive(Debug)]
+pub enum JKKX16Error {
+    /// Error when creating setup parameters
+    SetupError,
+    /// Happens when the infinity bit is set in an encoding point, but the rest of the bytes aren't correctly zero'd
+    InvalidPinError,
+    /// Error coming from `ark_serialize` upon deserialization
+    SerializationError(ark_serialize::SerializationError),
+    /// Error coming from `ark_ec` upon hashing to curve
+    HashingError(HashToCurveError),
+}
+
+impl std::error::Error for JKKX16Error {}
+
+impl fmt::Display for JKKX16Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            JKKX16Error::SetupError => 
+                write!(f, "Error during setup"),
+            JKKX16Error::InvalidPinError => 
+                write!(f, "Error recovering secret, likely due to invalid pin."),
+            JKKX16Error::SerializationError(ref err) => 
+                err.fmt(f),
+            JKKX16Error::HashingError(ref err) => 
+                err.fmt(f),
+        }
+    }
+}
 
 pub struct JKKX16 {
     _group: PhantomData<G1Config>,
@@ -87,7 +121,8 @@ impl PpssPcheme for JKKX16
         password: &[u8],
         rng: &mut R,
     ) -> Result<(Self::ClientState, Self::PrfInput), Error> {
-        let (blind, prf_input) = oprf_input(client_id, password, rng)?;
+        let (blind, prf_input) = oprf_input(client_id, password, rng)
+            .map_err(|e| JKKX16Error::HashingError(e))?;
 
         let state = ClientState { 
             blind_scalar: blind,
@@ -105,6 +140,7 @@ impl PpssPcheme for JKKX16
         input: &Self::PrfInput,
     ) -> Result<Self::PrfOutput, Error> {
         evaluate_prf(pp, seed, client_id, input)
+            .map_err(|e| JKKX16Error::SerializationError(e).into())
     }
 
     fn client_keygen<R: Rng>(
@@ -121,23 +157,25 @@ impl PpssPcheme for JKKX16
 
         let mut encrypted_shares = Vec::new();
         for (i, server_output) in server_responses.iter().enumerate() {
-            let prf_output: G1Affine = server_output.blinded_prf_output.mul(&state.blind_scalar.inverse().unwrap()).into();
+            let prf_output: G1Affine = server_output.blinded_prf_output
+                .mul(&state.blind_scalar.inverse().unwrap())
+                .into();
+
             // e := H(password || prf_output);
             let mask_i = hash_to_fr(
                 HashDomainSeparator::MaskDerivation as u8,
                 &[prf_output],
                 &[],
                 &[state.password.clone()]
-            )?;
+            ).map_err(|e| JKKX16Error::SerializationError(e))?;
+
             encrypted_shares.push((shares[i].0, shares[i].1 + mask_i));
         }
 
         // H3(0, s) in the paper
-        let hashed_secret = fr_to_32bytes(hash_to_fr(
-            HashDomainSeparator::DataKeyDerivation as u8,
-            &[],
-            &[secret],
-            &[])?
+        let hashed_secret = fr_to_32bytes(
+            hash_to_fr(HashDomainSeparator::DataKeyDerivation as u8, &[], &[secret], &[])
+            .map_err(|e| JKKX16Error::SerializationError(e))?
         );
         let mut r = [0u8; 16]; r.copy_from_slice(&hashed_secret[0..16]);
         let mut key = [0u8; 16]; key.copy_from_slice(&hashed_secret[16..32]);
@@ -151,7 +189,7 @@ impl PpssPcheme for JKKX16
             &[],
             &ys,
             &[state.password.to_vec(), r.to_vec()]
-        )?;
+        ).map_err(|e| JKKX16Error::SerializationError(e))?;
 
         let ctxt: Ciphertext<G1Projective> = Ciphertext { encrypted_shares, hash: c };
 
@@ -164,7 +202,8 @@ impl PpssPcheme for JKKX16
         password: &[u8],
         rng: &mut R
     ) -> Result<(Self::ClientState, Self::PrfInput), Error> {
-        let (blind, prf_input) = oprf_input(client_id, password, rng)?;
+        let (blind, prf_input) = oprf_input(client_id, password, rng)
+            .map_err(|e| JKKX16Error::HashingError(e))?;
 
         let state = ClientState { 
             blind_scalar: blind,
@@ -181,7 +220,8 @@ impl PpssPcheme for JKKX16
         client_id: &[u8],
         input: &Self::PrfInput,
     ) -> Result<Self::PrfOutput, Error> {
-        Ok(evaluate_prf(pp, seed, client_id, input)?)
+        evaluate_prf(pp, seed, client_id, input)
+            .map_err(|e| JKKX16Error::SerializationError(e).into())
     }
 
     fn client_reconstruct(
@@ -194,13 +234,14 @@ impl PpssPcheme for JKKX16
         let mut shares = Vec::new();
         for (i, server_output) in server_responses.iter().enumerate() {
             let prf_output: G1Affine = server_output.blinded_prf_output.mul(&state.blind_scalar.inverse().unwrap()).into();
+
             // e := H(password || prf_output);
             let mask_i = hash_to_fr(
                 HashDomainSeparator::MaskDerivation as u8,
                 &[prf_output],
                 &[],
                 &[state.password.clone()]
-            )?;
+            ).map_err(|e| JKKX16Error::SerializationError(e))?;
             
             let e_i = ciphertext.encrypted_shares[i];
             shares.push((e_i.0, e_i.1 - mask_i));
@@ -209,7 +250,11 @@ impl PpssPcheme for JKKX16
         // interpolate the shares to get the secret
         let secret = sss::recover(&shares);
 
-        let hashed_secret = fr_to_32bytes(hash_to_fr(HashDomainSeparator::DataKeyDerivation as u8, &[], &[secret], &[])?);
+        let hashed_secret = fr_to_32bytes(
+            hash_to_fr(
+                HashDomainSeparator::DataKeyDerivation as u8, &[], &[secret], &[]
+            ).map_err(|e| JKKX16Error::SerializationError(e))?
+        );
         let mut r = [0u8; 16]; r.copy_from_slice(&hashed_secret[0..16]);
         let mut key = [0u8; 16]; key.copy_from_slice(&hashed_secret[16..32]);
 
@@ -219,9 +264,11 @@ impl PpssPcheme for JKKX16
 
         let c = hash_to_fr(HashDomainSeparator::ReconstructionCheckDerivation as u8, &[], &ys, &[state.password.to_vec(), r.to_vec()])?;
 
-        assert_eq!(c, ciphertext.hash);
-
-        Ok(key)
+        if c == ciphertext.hash {
+            return Ok(key);
+        } else {
+            return Err(JKKX16Error::InvalidPinError.into());
+        }
     }
 
 }
@@ -230,9 +277,9 @@ fn oprf_input<R: Rng>(
     client_id: &[u8],
     password: &[u8],
     rng: &mut R
-) -> Result<(Fr, PrfInput<G1Projective>), Error> {
+) -> Result<(Fr, PrfInput<G1Projective>), HashToCurveError> {
     // hash the password to a group element
-    let password_hash: Affine::<G1Config> = hash_to_g1point(&password.to_vec());
+    let password_hash: Affine::<G1Config> = hash_to_g1point(&password.to_vec())?;
 
     let random_scalar = Fr::rand(rng);
     let blinded_prf_input = password_hash.mul(&random_scalar).into();
@@ -247,7 +294,7 @@ fn evaluate_prf(
     seed: &[u8; 32],
     client_id: &[u8],
     input: &PrfInput<G1Projective>
-) -> Result<PrfOutput<G1Projective>, Error> {
+) -> Result<PrfOutput<G1Projective>, ark_serialize::SerializationError> {
     let (client_secret_key, client_public_key) = {
         // TODO: do proper HKDF here. For now, Hash seed and client_id.
         // k := H(seed || client_id);
@@ -257,6 +304,7 @@ fn evaluate_prf(
             &[],
             &[seed.to_vec(), client_id.to_vec()]
         )?;
+        
         let client_public_key = pp.generator.mul(&client_secret_key).into();
 
         (client_secret_key, client_public_key)
@@ -275,22 +323,22 @@ const DST_G1: &str = "BLS_SIG_BLS12381G1_XMD:SHA-256_SSWU_RO_POP_";
 /// ([spec link](https://www.ietf.org/archive/id/draft-irtf-cfrg-bls-signature-05.html#section-1.3))
 /// A cryptographic hash function that takes as input an arbitrary octet string and returns a point on an
 /// elliptic curve. Functions of this kind are defined in [hash-to-curve-spec](https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-hash-to-curve-16).
-///
-/// Note: given we're using the "minimal-pubkey-size" variant of the spec, this function must output a point in G2.
-///
-/// XXX: this function doesn't take DST as an argument in the spec. It should!
-fn hash_to_g1point(msg: &Vec<u8>) -> Affine<G1Config> {
+fn hash_to_g1point(msg: impl AsRef<[u8]>) -> Result<Affine<G1Config>, HashToCurveError> {
     let g1_mapper = MapToCurveBasedHasher::<
         Projective<G1Config>,
         DefaultFieldHasher<Sha256, 128>,
         WBMap<G1Config>,
-    >::new(DST_G1.as_bytes())
-    .unwrap();
-    let q: Affine<G1Config> = g1_mapper.hash(msg).unwrap();
-    q
+    >::new(DST_G1.as_bytes())?;
+    let q: Affine<G1Config> = g1_mapper.hash(msg.as_ref())?;
+    Ok(q)
 }
 
-fn hash_to_fr(domain_separator: u8, affine_inputs: &[G1Affine], scalar_inputs: &[Fr], bytearray_inputs: &[Vec<u8>]) -> Result<Fr, Error> {
+fn hash_to_fr(
+    domain_separator: u8,
+    affine_inputs: &[G1Affine],
+    scalar_inputs: &[Fr],
+    bytearray_inputs: &[Vec<u8>]
+) -> Result<Fr, ark_serialize::SerializationError> {
     let mut hash_input = Vec::new();
 
     domain_separator.serialize_compressed(&mut hash_input)?;
@@ -316,7 +364,9 @@ fn hash_to_fr(domain_separator: u8, affine_inputs: &[G1Affine], scalar_inputs: &
 
 fn fr_to_32bytes(fr: Fr) -> [u8; 32] {
     let mut bytes = Vec::new();
-    fr.serialize_compressed(&mut bytes).unwrap();
+    fr
+        .serialize_compressed(&mut bytes)
+        .expect("scalars should be serializable");
     
     let mut padded_bytes = [0u8; 32];
     padded_bytes.copy_from_slice(&bytes);
